d695748e86ddcf7780b5ba4d8ad8a5cea0f5dc96
CRYZORE <cryzore@gmail.com>
2023-05-20 21:44:45 +0300

    version 1.3

--- app/src/main/java/com/slaythetrash/MainActivity.java
+++ app/src/main/java/com/slaythetrash/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 addFlyingCloud();
-                mHandler.postDelayed(this, 5000); // Запустить летящие облака каждые 5 секунд
+                mHandler.postDelayed(this, 5000);
             }
         };
         mHandler.post(mRunnable);
@@ -79,24 +79,20 @@ public class MainActivity extends AppCompatActivity {
         ImageView cloudImageView = new ImageView(this);
         int randomIndex = new Random().nextInt(CloudImages.length);
         cloudImageView.setImageResource(CloudImages[randomIndex]);
-
-        // Установите параметры размещения для облака, например, начальные координаты и размеры
-        int imageWidth = 200; // Ширина облака
-        int imageHeight = 200; // Высота облака
+        int imageWidth = 200;
+        int imageHeight = 200;
         int screenWidth = getResources().getDisplayMetrics().widthPixels;
-        int screenHeight = getResources().getDisplayMetrics().heightPixels; // Высота экрана устройства
-        int xPosition = -imageWidth+200; // Координата по оси X в левой части экрана
+        int screenHeight = getResources().getDisplayMetrics().heightPixels;
+        int xPosition = -imageWidth+200;
         int yPosition = new Random().nextInt(screenWidth / 2);
 
-        // Создание параметров размещения и установка координат для облака
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(imageWidth, imageHeight);
         layoutParams.leftMargin = xPosition;
         layoutParams.topMargin = yPosition;
         cloudImageView.setLayoutParams(layoutParams);
         cloudImageView.setTranslationZ(-2);
-        // Установите анимацию движения облака справа налево
         Animation flyAnimation = AnimationUtils.loadAnimation(this, R.anim.fly_animation);
-        flyAnimation.setDuration(20000); // Длительность анимации движения облака
+        flyAnimation.setDuration(20000);
         cloudImageView.startAnimation(flyAnimation);
 
         rootLayout = findViewById(R.id.layout);
@@ -108,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 addFallingTrash();
-                mHandler.postDelayed(this, 2000); // Запустить падение мусора каждые 2 секунды
+                mHandler.postDelayed(this, 2000);
             }
         };
         mHandler.post(mRunnable);
@@ -117,25 +113,21 @@ public class MainActivity extends AppCompatActivity {
         ImageView trashImageView = new ImageView(this);
         int randomIndex = new Random().nextInt(TrashImages.length);
         trashImageView.setImageResource(TrashImages[randomIndex]);
-        // Установите параметры размещения для изображения мусора, например, начальные координаты и размеры
-        int imageWidth = 200; // Ширина изображения мусора
-        int imageHeight = 200; // Высота изображения мусора
-        int screenWidth = getResources().getDisplayMetrics().widthPixels; // Ширина экрана устройства
-        int xPosition = new Random().nextInt(screenWidth - imageWidth); // Случайная координата по оси X
-        int yPosition = -imageHeight; // Координата по оси Y в верхней части экрана
+        int imageWidth = 200;
+        int imageHeight = 200;
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+        int xPosition = new Random().nextInt(screenWidth - imageWidth);
+        int yPosition = -imageHeight;
 
-        // Создание параметров размещения и установка координат для изображения мусора
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(imageWidth, imageHeight);
         layoutParams.leftMargin = xPosition;
         layoutParams.topMargin = yPosition;
         trashImageView.setLayoutParams(layoutParams);
         trashImageView.setTranslationZ(-1);
-        // Установите анимацию падения и вращения по оси Y
         Animation fallAnimation = AnimationUtils.loadAnimation(this, R.anim.fall_animation);
         fallAnimation.setDuration(20000);
         trashImageView.startAnimation(fallAnimation);
         rootLayout = findViewById(R.id.layout);
-        // Добавьте изображение мусора в вашу разметку или контейнер, например, корневой Layout
         root.addView(trashImageView);
     }
     @Override
@@ -174,7 +166,6 @@ public class MainActivity extends AppCompatActivity {
                     btnContinue.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            // Действия при нажатии на кнопку "Продолжить"
                             startActivity(new Intent(MainActivity.this, Game.class));
                             finish();
                         }
@@ -197,14 +188,6 @@ public class MainActivity extends AppCompatActivity {
         notifications1 = register_window.findViewById(R.id.layout);
         final MaterialEditText login = register_window.findViewById(R.id.loginField);
         final MaterialEditText password = register_window.findViewById(R.id.passwordField);
-
-        /*     dialog.setNegativeButton("Закрыть", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int which) {
-                dialogInterface.dismiss();
-            }
-        });
-        */
         final AlertDialog registerDialog = dialog.create();
         Button SkipButton = register_window.findViewById(R.id.skipButton);
         SkipButton.setOnClickListener(new View.OnClickListener() {
@@ -245,38 +228,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-     /*   dialog.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int which) {
-                if (TextUtils.isEmpty(login.getText().toString())) {
-                    Snackbar.make(root, "Введите почту", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-                if (password.getText().toString().length() < 5) {
-                    Snackbar.make(root, "Пароль должен содержать не менее 5-ти символов", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-
-                //регистрация пользователя
-                auth.createUserWithEmailAndPassword(login.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        User user = new User();
-                        user.setLogin(login.getText().toString());
-                        user.setPassword(password.getText().toString());
-
-                        users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Snackbar.make(root, "Пользователь добавлен", Snackbar.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                });
-
-            }
-        });
-*/
         TextView loginLinkTextView = register_window.findViewById(R.id.loginLinkTextView);
         loginLinkTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -305,41 +256,7 @@ public class MainActivity extends AppCompatActivity {
         notifications2 = sign_window.findViewById(R.id.layout);
         final MaterialEditText login = sign_window.findViewById(R.id.loginField);
         final MaterialEditText password = sign_window.findViewById(R.id.passwordField);
-   /*     dialog.setNegativeButton("Закрыть", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int which) {
-                dialogInterface.dismiss();
-            }
-        });
-        dialog.setPositiveButton("Ввести", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int which) {
-                if(TextUtils.isEmpty(login.getText().toString())) {
-                    Snackbar.make(root, "Введите логин", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-                if(password.getText().toString().length() < 5) {
-                    Snackbar.make(root, "Пароль должен содержать не менее 5-ти символов", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-                auth.signInWithEmailAndPassword(login.getText().toString(), password.getText().toString())
-                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                            @Override
-                            public void onSuccess(AuthResult authResult) {
-                                startActivity(new Intent(MainActivity.this, Game.class));//Сделать выбор уровней
-                                finish();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Snackbar.make(root, "Ошибка авторизации. " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
-                            }
-                        });
-
-            }
-
-        });
-     */        final AlertDialog registerDialog = dialog.create();
+        final AlertDialog registerDialog = dialog.create();
         Button SkipButton = sign_window.findViewById(R.id.skipButton);
         SkipButton.setOnClickListener(new View.OnClickListener() {
             @Override
